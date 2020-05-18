8e0f1820503a998ecef8f2428bbe9c8a73fb3c75
dy956122 <dy956122@gmail.com>
2020-05-19 01:25:46 +0800

    新增遊戲標題畫面及場景連結

---
+++ back_Prototype_01/Assets/CSharp/Menu/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public string SceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void NextScene() {//選擇下一關
+        Application.LoadLevel(SceneName);
+    } //選擇下一關 結束
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
