6689065de2341a86eb1632c8382da711479581be
dy956122 <dy956122@gmail.com>
2020-05-30 23:54:41 +0800

    20200530 進度
    1. 追加 遊戲主畫面 音效控制
        另外螢幕調整用的地方,
        是用暫時的東西代替
    2. 遊戲進行中，右邊的提示,
        取消 關閉按鈕

---
+++ back_Prototype_01/Assets/CSharp/Game_Rule/Menu.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    #region 屬性與欄位設定
+
+    [Header("遊戲開頭音樂"), Tooltip("遊戲開頭音樂")]
+    /// <summary>
+    /// 遊戲開頭音樂
+    /// </summary>
+    public Slider GameThemeMusic;
+
+    [Header("音樂開關"), Tooltip("音樂開關")]
+    /// <summary>
+    /// 音樂開關按鈕
+    /// </summary>
+    bool MusicSwith;
+
+    [Header("音樂開關按鈕"), Tooltip("音樂開關按鈕")]
+    /// <summary>
+    /// 音樂開關按鈕
+    /// </summary>
+    public Image MusicControlButton;
+
+
+    [Header("聲音開啟圖"), Tooltip("聲音開啟圖")]
+    /// <summary>
+    /// 聲音開啟圖
+    /// </summary>
+    public Sprite MusicOpenButton;
+
+    [Header("聲音關閉圖"), Tooltip("聲音關閉圖")]
+    /// <summary>
+    /// 聲音開啟圖
+    /// </summary>
+    public Sprite MusicCloseButton;
+
+    [Header("螢幕大小調整"), Tooltip("螢幕大小調整")]
+    /// <summary>
+    /// 螢幕大小調整
+    /// </summary>
+    public Dropdown ScreenSize;
+
+    #endregion 屬性與欄位設定 結束
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ControlThemeMusic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioListener.volume = GameThemeMusic.value;
+        ScreenSizeAction();
+    }
+
+
+    #region 方法創建
+    public void ScreenSizeAction() // 螢幕調整
+    {
+        switch (ScreenSize.value) // 設定螢幕大小
+        {
+            case 0:
+                Screen.SetResolution(1024, 768, false);
+                break;
+
+            case 1:
+                Screen.SetResolution(1280, 720, false);
+                break;
+
+            case 2:
+                Screen.SetResolution(1920, 720, false);
+                break;
+        } // 設定螢幕大小 結束
+    } // 螢幕調整 結束
+
+    public void ChangeThemeMusic() // 調整音量功能
+    {
+        if (GameThemeMusic.value == 0)
+        {
+            MusicSwith = true;
+            ControlThemeMusic();
+        }
+        else
+        {
+            MusicSwith = false;
+            ControlThemeMusic();
+        }
+    }  // 調整音量功能 結束
+
+    public void ControlThemeMusic() // 音量開關
+    {
+        MusicSwith = !MusicSwith;
+        if (MusicSwith == true)
+        {
+            AudioListener.pause = false;
+
+            MusicControlButton.sprite = MusicOpenButton;
+        }
+        else
+        {
+            AudioListener.pause = true;
+
+            MusicControlButton.sprite = MusicCloseButton;
+        }
+    } // 音量開關 結束
+
+    public void Exit() // 離開
+    {
+        Application.Quit();
+    } // 離開 結束
+
+    #endregion 方法創建 結束
+
+}
---
+++ back_Prototype_01/Assets/CSharp/Game_Rule/SceneChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    [Header("進入指定名稱的關卡"), Tooltip("進入指定名稱的關卡")]
+    /// <summary>
+    /// 進入指定名稱的關卡
+    /// </summary>
+    public string SceneName;
+
+    [Header("音量儲存的字串名"), Tooltip("音量儲存的字串名")]
+    /// <summary>
+    /// 指定音量儲存的字串名稱
+    /// </summary>
+    string SaveAudioSlider = "SaveAudioSlider";
+
+
+
+
+    public void NextScene()
+    {//選擇下一關
+        PlayerPrefs.SetFloat(SaveAudioSlider, AudioListener.volume);
+        Application.LoadLevel(SceneName);
+
+
+    } //選擇下一關 結束
+
+}
