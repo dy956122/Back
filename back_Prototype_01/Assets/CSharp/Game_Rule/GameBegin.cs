3d567164da93f43723f1f72a99dd549424310314
dy956122 <dy956122@gmail.com>
2020-07-14 02:16:43 +0800

    釋出測試版
    1. 使用Dotween製作動畫
    2. 增加地形變化
    3. 追加布偶(尚無AI)

---
+++ back_Prototype_01/Assets/CSharp/Game_Rule/GameBegin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameBegin : MonoBehaviour
+{
+    public string sceneName;
+    public int animationTime;
+
+    void Start()
+    {
+        Invoke("LoadScene", 6.0f);
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ back_Prototype_01/Assets/CSharp/Game_Rule/MonsterCreater.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MonsterCreater : MonoBehaviour
+{
+    public GameObject Voodoo;
+    public GameObject CreaterPonit;
+
+    void Update()
+    {
+        Invoke("Create", 1);
+
+    }
+
+
+    public void Create()
+    {
+        Instantiate(Voodoo, Vector3.zero, Quaternion.identity);
+    }
+
+}
