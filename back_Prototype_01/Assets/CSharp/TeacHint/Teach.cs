feb0f94559f532307896720a2bb4d67d0f32ee56
dy956122 <dy956122@gmail.com>
2020-08-18 07:18:37 +0800

    新增教學關卡

---
+++ back_Prototype_01/Assets/CSharp/TeacHint/Teach.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+// 教學關專用的腳本
+public class Teach : MonoBehaviour
+{
+    /*
+    [SerializeField]
+    private GameObject[] teachButton, teachText;
+
+    private int teachButtonNum = 0;
+
+    public void TeachButtonVanish()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            teachButton[teachButtonNum].SetActive(false);
+            teachText[teachButtonNum].SetActive(false);
+            teachButtonNum++;
+        }
+    }
+
+    public void TeachButtonAppear()
+    {
+        if (Input.GetMouseButtonDown(0) && teachButtonNum <=4)
+        {
+            teachButton[teachButtonNum+1].SetActive(true);
+            teachText[teachButtonNum+1].SetActive(true);
+        }
+    }
+    */
+
+    public void VanishButton()
+    {
+
+    }
+
+    private void Awake()
+    {
+       // teachButton[teachButtonNum].SetActive(true);
+       // teachText[teachButtonNum].SetActive(true);
+    }
+
+    private void Update()
+    {
+       // TeachButtonVanish();
+       // TeachButtonAppear();
+    }
+}
